1074556ab8645e5b6f29492588d73fadb1c90431
Anvar571 <abdurashidovanvar52@gmail.com>
2025-04-04 00:20:26 +0500

    feat: simple server

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
-function main() {
-    return "Hello, World!";
+import * as net from "node:net";
+
+function newConn(socket: net.Socket): void {
+    console.log('new connection', socket.remoteAddress, socket.remotePort);
+
+    socket.on('end', () => {
+        console.log('EOF.');
+    });
+    socket.on('data', (data: Buffer) => {
+        console.log('data:', data);
+        socket.write(data);
+
+        if (data.includes('q')) {
+            console.log('closing.');
+            socket.end();
+        }
+    });
 }
 
-console.log(main());
+const server = net.createServer();
+
+server.on("connection", newConn);
+
+server.listen(8080, () => {
+    console.log("Server listening on port 8080");
+});
